8da67a8467458558e5eb88c6405d0b8f74837a71
Sid Tickle <32840198+siddtickle@users.noreply.github.com>
2021-06-01 11:46:44 -0400

    added get requests to server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,43 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 const port = 8000;
-const cors = require('cors')
-const db = require('./firebase')
-const axios = require('axios');
+const cors = require("cors");
+const db = require("./firebase");
+const axios = require("axios");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json());
+app.use(cors());
+
+app.get("/student/directory", async (req, res) => {
+  const snapshot = await db.collection("students").get();
+
+  let studentList = [];
+
+  snapshot.forEach((doc) => {
+    studentList.push({ ...doc.data(), id: doc.id });
+  });
+
+  res.send(studentList);
+  res.sendStatus(200);
+});
+
+app.get("/teacher/directory", async (req, res) => {
+  const snapshot = await db.collection("teachers").get();
+
+  let teacherList = [];
+
+  snapshot.forEach((doc) => {
+    teacherList.push({ ...doc.data(), id: doc.id });
+  });
+
+  res.send(teacherList);
+  res.sendStatus(200);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
