8c3c28f57770761ffd47de819b5663edb64bb1a4
Yaseen Bhuiyan <ywb9znu@virginia.edu>
2021-06-01 16:12:38 -0400

    added class get req to server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,30 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 const port = 8000;
-const cors = require('cors')
-const db = require('./firebase')
-const axios = require('axios');
+const cors = require("cors");
+const db = require("./firebase");
+const axios = require("axios");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json());
+app.use(cors());
+
+app.get("/class/directory", async (req, res) => {
+  const snapshot = await db.collection("classes").get();
+
+  let classList = [];
+
+  snapshot.forEach((doc) => {
+    classList.push({ ...doc.data(), id: doc.id });
+  });
+
+  res.send(classList);
+  res.sendStatus(200);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
