5b8acf4614485da4ec8840b3a6ebd3f17d5c87dc
Khushi Chawla <emailkhushic@gmail.com>
2021-06-01 14:15:40 -0400

    get specific class by teacher id

--- server/index.js
+++ server/index.js
@@ -9,6 +9,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+app.get("/class", async(req, res) => {
+  var teacher_id = req.query.teacher_id;
+  console.log(teacher_id);
+  db.collection("classes").where("teacher", "==", teacher_id).get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        res.send(doc.data());
+    });
+  })
+  .catch((error) => {
+      console.log("Error getting documents: ", error);
+  });
+
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
