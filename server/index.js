4a313b14e9b685a70f6240b77cdbf3e3ee833eb0
Khushi Chawla <emailkhushic@gmail.com>
2021-06-02 13:37:22 -0400

    fix get_class get request

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const port = 8000;
-const cors = require('cors')
-const db = require('./firebase')
+const port = 5000;
+const cors = require('cors');
+const db = require('./firebase');
 var admin = require('firebase-admin');
 const axios = require('axios');
 app.use(express.json())
@@ -16,18 +16,15 @@ app.get("/classes/get_class", async(req, res) => {
   var class_id = req.query.class_id;
   console.log(class_id);
   var classes = db.collection("classes").doc(class_id);
-
-  /*db.collection("classes").where("teacher", "==", teacher_id).get()
-  .then((querySnapshot) => {
-    res.send(querySnapshot[1]);
-    querySnapshot.forEach((doc) => {
-      res.sendStatus(200);
-        //res.send({id: doc.id(), data: doc.data()});
-    });
-  })
-  .catch((error) => {
-      console.log("Error getting documents: ", error);
-  });*/
+  classes.get().then((doc) => {
+    if (doc.exists) {
+        res.send(doc.data());
+    } else {
+        res.sendStatus(404);
+    }
+  }).catch((error) => {
+    res.sendStatus(404);
+  });
 });
 
 app.post("/classes/add_student", async(req, res) => {
