39ee89c9d8362ceee74c2a2140b616bb732c9f09
Khushi Chawla <emailkhushic@gmail.com>
2021-06-02 10:07:21 -0400

    delete request to remove student from a class

--- server/index.js
+++ server/index.js
@@ -3,25 +3,51 @@ const app = express();
 const port = 8000;
 const cors = require('cors')
 const db = require('./firebase')
+var admin = require('firebase-admin');
 const axios = require('axios');
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.get("/class", async(req, res) => {
+app.get("/classes/get_class", async(req, res) => {
   var teacher_id = req.query.teacher_id;
   console.log(teacher_id);
   db.collection("classes").where("teacher", "==", teacher_id).get()
   .then((querySnapshot) => {
+    res.send(querySnapshot[1]);
     querySnapshot.forEach((doc) => {
-        res.send(doc.data());
+      res.sendStatus(200);
+        //res.send({id: doc.id(), data: doc.data()});
     });
   })
   .catch((error) => {
       console.log("Error getting documents: ", error);
   });
+});
 
+app.post("/classes/add_student", async(req, res) => {
+  console.log(req.body);
+  const {doc_id, student_id} = req.body;
+  var classes = db.collection("classes").doc(doc_id);
+
+  var arrUnion = classes.update({
+    students: admin.firestore.FieldValue.arrayUnion(student_id)
+  });
+  res.sendStatus(200);
+});
+
+app.delete("/classes/remove_student", async(req, res) =>{
+  console.log(req.body);
+  const {doc_id, student_id} = req.body;
+  var classes = db.collection("classes").doc(doc_id);
+
+  var arrUnion = classes.update({
+    students: admin.firestore.FieldValue.arrayRemove(student_id)
+  });
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
