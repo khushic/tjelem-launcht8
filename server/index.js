8dfe8cc51ca690a1a6e6f6b3282c099e0da3e4f4
Sid Tickle <32840198+siddtickle@users.noreply.github.com>
2021-06-03 11:57:36 -0400

    completed teacher directory

--- server/index.js
+++ server/index.js
@@ -136,6 +136,86 @@ app.get("/teacher/directory", async (req, res) => {
   // res.sendStatus(200);
 });
 
+app.delete("/teacher/directory/delete", async (req, res) => {
+  const { id } = req.body;
+  // console.log(db.collection("students").doc(id));
+
+  const resp = await db.collection("teachers").doc(id).delete();
+  console.log(`Deleted element with id: ${id}`);
+
+  const snapshot = await db.collection("teachers").get();
+
+  let teacherList = [];
+
+  snapshot.forEach((doc) => {
+    teacherList.push({ ...doc.data(), id: doc.id });
+  });
+  res.send(teacherList);
+
+  // res.sendStatus(200);
+  //add get to to produce response of array for instant update
+});
+
+app.post("/teacher/directory/add", async (req, res) => {
+  // console.log(req.body);
+
+  const { address, birthday, email, first_name, gender, grade, last_name } =
+    req.body;
+
+  const resp = await db.collection("teachers").add({
+    address,
+    birthday,
+    email,
+    first_name,
+    gender,
+    grade,
+    last_name,
+  });
+
+  console.log(`Added element with id: ${resp.id}`);
+
+  const snapshot = await db.collection("teachers").get();
+
+  let teacherList = [];
+
+  snapshot.forEach((doc) => {
+    teacherList.push({ ...doc.data(), id: doc.id });
+  });
+  res.send(teacherList);
+
+  // res.sendStatus(200);
+});
+
+app.put("/teacher/directory/edit", async (req, res) => {
+  const { address, birthday, email, first_name, gender, grade, last_name, id } =
+    req.body;
+
+  console.log("id: ", req.body.id);
+
+  const resp = await db.collection("teachers").doc(id).update({
+    address,
+    birthday,
+    email,
+    first_name,
+    gender,
+    grade,
+    last_name,
+  });
+
+  console.log(`Edited element with id: ${id}`);
+
+  const snapshot = await db.collection("teachers").get();
+
+  let teacherList = [];
+
+  snapshot.forEach((doc) => {
+    teacherList.push({ ...doc.data(), id: doc.id });
+  });
+  res.send(teacherList);
+
+  // res.sendStatus(200);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import StudentDirectory from "./StudentDirectory";
+import TeacherDirectory from "./TeacherDirectory";
 import React, { useState, useEffect } from "react";
 
 function App() {
   const [students, setStudents] = useState(null);
+  const [teachers, setTeachers] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/student/directory")
@@ -11,12 +13,20 @@ function App() {
       .then((resp) => setStudents(resp));
   }, [setStudents]);
 
+  useEffect(() => {
+    fetch("http://localhost:8000/teacher/directory")
+      .then((resp) => resp.json())
+      .then((resp) => setTeachers(resp));
+  }, [setTeachers]);
+
   if (!students) return <h1>loading...</h1>;
+  if (!teachers) return <h1>loading...</h1>;
   // console.log("test", students);
 
   return (
     <div>
       <StudentDirectory students={students} setStudents={setStudents} />
+      <TeacherDirectory teachers={teachers} setTeachers={setTeachers} />
     </div>
   );
 }
